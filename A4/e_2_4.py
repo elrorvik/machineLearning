298c27ab785c8109cae520bac3db29b376d30399
Ella-Lovise <elrorvik@stud.ntnu.no>
2018-04-13 22:16:31 +0200

    not time for this

--- A4/e_2_4.py
+++ A4/e_2_4.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sigmoid(z): 
+def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
 
 def classify(w1,w2,x):
@@ -12,7 +12,7 @@ def classify(w1,w2,x):
     return 0 if (z2<0.5) else 1
 
 def delta_loss(y_t,y,w,x):
-    return 
+    return
 
 def update_w(w,alpha,delta_L):
     return w - alpha*delta_L
@@ -22,16 +22,17 @@ def train(x,y_t,w1,w2):
     epochs = 60000
     alpha = 1
     for j in range(epochs):
+        #forward
         z0 = X
         z1 = sigmoid(np.dot(z0,w1)) # hidden 1
-        z2 = sigmoid(np.dot(z1,w2)) # hidden 2
+        z2 = sigmoid(np.dot(z1,w2)) # output
 
         z2_delta = -(y - z2)*(z2*(1-z2))
         z1_delta = z2_delta.dot(w2.T) * (z1 * (1-z1))
 
         delta_loss_w2 = z1.T.dot(z2_delta)
         delta_loss_w1 = z0.T.dot(z1_delta)
-        
+
         w2 -= alpha*delta_loss_w2
         w1 -= alpha*delta_loss_w1
     return w1,w2
@@ -53,6 +54,3 @@ print(test, " classified: ", classify(w1,w2,test))
 
 test = [1,1]
 print(test, " classified: ", classify(w1,w2,test))
-
-
-
