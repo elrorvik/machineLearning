95e956fff8c1d174d5b9679c530104a6ee6aa040
Ella-Lovise <elrorvik@stud.ntnu.no>
2018-04-28 23:59:41 +0200

    Made labels and image fit togheter  and split train and test

--- A5/OCR.py
+++ A5/OCR.py
@@ -1,7 +1,11 @@
 from skimage.io import imread_collection, imshow, concatenate_images
 from skimage import io
 import numpy as np
+import skimage
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from skimage.feature import hog
+from skimage.color import rgb2gray
 
 def label_to_letter(label):
     return chr(label+97)
@@ -10,26 +14,35 @@ def letter_to_label(letter):
     return int(ord(letter)-97)
 
 def get_image(folder_path):
-    #creating a collection with the available images
-    col = imread_collection(folder_path)
+    col = imread_collection(folder_path) #creating a collection with the available images
     train = concatenate_images(col)
     label = np.zeros((train.shape[0],1))
 
-    for (image, file_name,i) in zip(train, col.files,range(train.shape[0])):
+    for (file_name,i) in zip(col.files,range(train.shape[0])):
         label[i] = letter_to_label(file_name[14])
     return label,train
 
+def split_train_test(y,X):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+    return X_train,X_test,y_train,y_test
 
 
-#your path
 col_dir = 'chars74k-lite/*/*.jpg'
-label,train = get_image(col_dir)
+label,image = get_image(col_dir)
+
+train_images,test_images,train_labels,test_labels = split_train_test(label,image)
+
+#print(train_images.shape)
+#for i in range(train_images.shape[0]):
+#    train_images[i] = rgb2gray(train_images[i])
+
+#df= hog(train_images, orientations=8, pixels_per_cell=(5,5), cells_per_block=(2, 2))
 
 
 plt.figure()
-plt.imshow(train[4000], cmap=plt.cm.gray)
+plt.imshow(train_images[4000])
 plt.figure()
-plt.imshow(train[2000],cmap=plt.cm.gray)
-print(label_to_letter(label[4000]))
-print(label_to_letter(label[2000]))
+plt.imshow(train_images[10])
+print(label_to_letter(train_labels[4000]))
+print(label_to_letter(train_labels[10]))
 plt.show()
